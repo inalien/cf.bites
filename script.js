352326fcb7e109fe8c49c0549962b909c146ed69
Jhon <jhon.inalien@gmail.com>
2024-02-11 13:12:11 -0500

    react

--- script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('emailForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-    var email = document.getElementById('email').value;
-    // Here, you would typically send the email to your server or a third-party email service
-    alert('Thank you for subscribing!');
-});
